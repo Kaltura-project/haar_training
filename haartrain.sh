24931708ed90c6d4b207fa34baca5ed46c8d9e6c
Vijay Pillai <vijay.pillai397@gmail.com>
2017-10-18 23:07:13 -0400

    fixed dir

--- haartrain.sh
+++ haartrain.sh
@@ -76,7 +76,7 @@ function setup {
 	find $NEWNEG -iname *.jpg > negatives.txt
 	mkdir "samples"
 	perl bin/sonots/createtrainsamples.pl positives.txt negatives.txt samples $NEGSAMPLES  "opencv_createsamples -bgcolor 0 -bgthresh 0 -maxxangle 1.1 -maxyangle 1.1 maxzangle 0.5 -maxidev 40 -w 40 -h 40"
-	python3 mergevec/mergevec.py -v samples -o train.vec
+	python3 bin/mergevec/mergevec.py -v samples -o train.vec
 }
 
 function train {
